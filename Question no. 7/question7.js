a1b1a76f28fa72526c4ecff15e2dbc63e907f3a2
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-03-09 19:40:16 -0800

    5 questions completed chp 14 - 16

---
+++ Question no. 7/question7.js
@@ -0,0 +1,16 @@
+document.write ("<h1> Declare and Initialize an array and store available <br> education qualifications in Pakistan (e.g. SSC, HSC, BCS, <br> BS, BCOM, MS, M. Phil., PhD). Show the listed <br> qualifications in your browser like: </h1><br>")
+
+// It's length is 8
+// It's type index is 7 bcz in index counting starts from 0
+var educationQualification = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phill.", "PHD"]
+
+document.write ("<h2> Qualifications </h2>")
+
+document.write ("<p> 1) "+ educationQualification[0]  + "</p>")
+document.write ("<p> 2) "+ educationQualification[1]  + "</p>")
+document.write ("<p> 3) "+ educationQualification[2]  + "</p>")
+document.write ("<p> 4) "+ educationQualification[3]  + "</p>")
+document.write ("<p> 5) "+ educationQualification[4]  + "</p>")
+document.write ("<p> 6) "+ educationQualification[5]  + "</p>")
+document.write ("<p> 7) "+ educationQualification[6]  + "</p>")
+document.write ("<p> 8) "+ educationQualification[7]  + "</p>")
