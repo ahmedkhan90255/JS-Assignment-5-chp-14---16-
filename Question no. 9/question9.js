840accc2b822c4a1bdd8437763817f607723c601
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-03-09 20:28:33 -0800

    7 questions completed chp 14-16

---
+++ Question no. 9/question9.js
@@ -0,0 +1,27 @@
+document.write ("<h2>Ques1. Initialize an array with color names. Display the array <br> elements in your browser.</h2>")
+
+var colors = ["Red ", "Blue ", "Green ", "Purple ", "Orange "]
+document.write ("<p> Ans1: " + colors + "</p>")
+
+var inputColor1 = prompt ("Which Color You want to add at the begining.")
+
+document.write ("<h2> a. Ask the user what color he/she wants to add to the <br> beginning & add that color to the beginning of the array. </h2>")
+colors = [inputColor1 + "," + " Red ", "Blue ", "Green ", "Purple ", "Orange "]
+document.write ("<p> Ans: " + colors + "</p>")
+
+document.write ("<h2> b. Ask the user what color he/she wants to add to the end <br> & add that color to the end of the array. </h2>")
+var inputColor2 = prompt ("Which Color You want to add at the end")
+colors.push(inputColor2)
+document.write ("<p> Ans: " + colors + "</p>")
+
+document.write ("<h2> c. Add two more color to the beginning of the array. </h2>")
+colors.unshift (" Crimson ", "Seagreen ")
+document.write ("<p> Ans: " + colors + "</p>")
+
+document.write ("<h2> d. Delete the first color in the array <h2>")
+colors.shift ()
+document.write ("<p> Ans: " + colors + "</p>")
+
+document.write ("<h2> e. Delete the last color in the array.<h2>")
+colors.pop ()
+document.write ("<p> Ans: " + colors + "</p>")
